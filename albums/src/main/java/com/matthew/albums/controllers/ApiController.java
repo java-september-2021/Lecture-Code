4c82fea08247aff0068a2b903b346b7fc4301525
Matthew Schiller <mschiller@codingdojo.com>
2021-09-24 09:04:08 -0700

    albums front end

---
+++ albums/src/main/java/com/matthew/albums/controllers/ApiController.java
@@ -0,0 +1,41 @@
+package com.matthew.albums.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.matthew.albums.models.Album;
+import com.matthew.albums.services.AlbumService;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+	@Autowired
+	private AlbumService aService;
+	
+	@GetMapping("")
+	public List<Album> index(){
+		return this.aService.getAllAlbums();
+	}
+	
+	@GetMapping("/{id}")
+	public Album getOne(@PathVariable("id") Long id) {
+		return this.aService.getOneAlbum(id);
+	}
+	
+	@PostMapping("/create")
+	public Album create(Album album) {
+		return this.aService.createAlbum(album);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public String delete(@PathVariable("id") Long id) {
+		return this.aService.deleteAlbum(id);
+	}
+}
